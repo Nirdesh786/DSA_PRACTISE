0e5bd4d71e671b559cf30e2a7dafcf5b8787e315
Nirdesh786 <152304390+Nirdesh786@users.noreply.github.com>
2026-04-23 11:43:15 +0530

    Time: 14 ms (31.52%) | Memory: 46.5 MB (23.54%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.java
+++ 125-valid-palindrome/valid-palindrome.java
@@ -4,14 +4,13 @@ class Solution {
         String result = s.replaceAll("[^a-zA-Z0-9]","").toLowerCase();
         int start = 0;
         int end = result.length()-1;
-        for(int i=0;i<result.length();i++){
-           if(result.charAt(start)==result.charAt(end)){
-            start++;
-            end--;
-           }else{
-            return false;
-           }
-
+        for(int i=start;i<end;i++){
+            if(result.charAt(start)==result.charAt(end)){
+                start++;
+                end--;
+            }else{
+                return false;
+            }
         }
         return true;
     }
