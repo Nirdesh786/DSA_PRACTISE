92f7d6bb75e9a5360bae6ccba3275004e4f38bfa
Nirdesh786 <152304390+Nirdesh786@users.noreply.github.com>
2026-08-17 22:53:23 +0530

    Time: 9 ms (14.24%) | Memory: 48 MB (84.58%) - LeetSync

--- 240-search-a-2d-matrix-ii/search-a-2d-matrix-ii.java
+++ 240-search-a-2d-matrix-ii/search-a-2d-matrix-ii.java
@@ -3,19 +3,19 @@ class Solution {
         int row= matrix.length;
         int col = matrix[0].length;
 
-        int[] arr = new int[row*col];
-        int index = 0;
-        for(int i=0;i<row;i++){
-            for(int j=0;j<col;j++){
-                arr[index++] = matrix[i][j];
-            }
-        }
-        index = 0;
-        for(int i=0;i<row;i++){
-            for(int j=0;j<col;j++){
-                 matrix[i][j]=arr[index++];
-            }
-        }
+        // int[] arr = new int[row*col];
+        // int index = 0;
+        // for(int i=0;i<row;i++){
+        //     for(int j=0;j<col;j++){
+        //         arr[index++] = matrix[i][j];
+        //     }
+        // }
+        // index = 0;
+        // for(int i=0;i<row;i++){
+        //     for(int j=0;j<col;j++){
+        //          matrix[i][j]=arr[index++];
+        //     }
+        // }
         for(int i=0;i<row;i++){
             for(int j=0;j<col;j++){
                 if(matrix[i][j]==target){
