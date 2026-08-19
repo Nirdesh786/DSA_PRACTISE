419b89ae86bef86bec45a523bca3c94a5bf3e4c3
Nirdesh786 <152304390+Nirdesh786@users.noreply.github.com>
2026-08-19 15:40:25 +0530

    Time: 19 ms (19.74%) | Memory: 43.8 MB (59.17%) - LeetSync

---
+++ 205-isomorphic-strings/isomorphic-strings.java
@@ -0,0 +1,39 @@
+import java.util.*;
+
+class Solution {
+    public boolean isIsomorphic(String s, String t) {
+
+        if (s.length() != t.length()) {
+            return false;
+        }
+
+        HashMap<Character, Character> map1 = new HashMap<>();
+        HashMap<Character, Character> map2 = new HashMap<>();
+
+        for (int i = 0; i < s.length(); i++) {
+
+            char a = s.charAt(i);
+            char b = t.charAt(i);
+
+            // Check s -> t mapping
+            if (map1.containsKey(a)) {
+                if (map1.get(a) != b) {
+                    return false;
+                }
+            } else {
+                map1.put(a, b);
+            }
+
+            // Check t -> s mapping
+            if (map2.containsKey(b)) {
+                if (map2.get(b) != a) {
+                    return false;
+                }
+            } else {
+                map2.put(b, a);
+            }
+        }
+
+        return true;
+    }
+}
