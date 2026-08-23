5e3b8da93713a57aab46a93e8bc4a817ccc6e076
Nirdesh786 <152304390+Nirdesh786@users.noreply.github.com>
2026-08-23 23:50:06 +0530

    Time: 18 ms (46.41%) | Memory: 43.6 MB (69.55%) - LeetSync

--- 5-longest-palindromic-substring/longest-palindromic-substring.java
+++ 5-longest-palindromic-substring/longest-palindromic-substring.java
@@ -1,40 +1,31 @@
 class Solution {
     public String longestPalindrome(String s) {
-          if (s == null || s.length() < 1)
-            return "";
-
         int start = 0;
-        int end = 0;
+        int maxLength = 0;
 
-        for (int i = 0; i < s.length(); i++) {
+        for(int i=0;i<s.length();i++){
 
-            int len1 = expand(s, i, i);       // odd length
-
-            int len2 = expand(s, i, i + 1);   // even length
-
-            int len = Math.max(len1, len2);
-
-            if (len > end - start) {
-
-                start = i - (len - 1) / 2;
-
-                end = i + len / 2;
+            int left = i;
+            int right = i;
+            while(left>=0 && right <s.length() && s.charAt(left)==s.charAt(right)){
+                if(right -left+1>maxLength){
+                    start = left;
+                    maxLength = right -left +1;
+                }
+                left--;
+                right++;
+            }
+            left = i;
+            right  = i+1;
+            while(left>=0 && right <s.length() && s.charAt(left)==s.charAt(right)){
+                if(right -left+1>maxLength){
+                    start = left;
+                    maxLength = right -left +1;
+                }
+                left--;
+                right++;
             }
         }
-
-        return s.substring(start, end + 1);
+        return s.substring(start,start+maxLength);
     }
-
-    private int expand(String s, int left, int right) {
-
-        while (left >= 0 &&
-               right < s.length() &&
-               s.charAt(left) == s.charAt(right)) {
-
-            left--;
-            right++;
-        }
-
-        return right - left - 1;
-    }
-    }
+}
