186f08b96e27668c7450a094affeb52c84898bd4
Nirdesh786 <152304390+Nirdesh786@users.noreply.github.com>
2026-09-07 12:47:29 +0530

    Time: 2 ms (99.71%) | Memory: 44.9 MB (76.35%) - LeetSync

--- 90-subsets-ii/subsets-ii.java
+++ 90-subsets-ii/subsets-ii.java
@@ -1,34 +1,24 @@
 class Solution {
     public List<List<Integer>> subsetsWithDup(int[] nums) {
-        List<List<Integer>> ans = new ArrayList<>();
         Arrays.sort(nums);
-
-        generate(0, nums, new ArrayList<>(), ans);
-
+        List<List<Integer>> ans = new ArrayList<>();
+        generate(0,nums,new ArrayList<>(),ans);
         return ans;
     }
-
-    private void generate(int index, int[] nums,
-                          List<Integer> current,
-                          List<List<Integer>> ans) {
-
-        if (index >= nums.length) {
+    private void generate(int index,int[] nums,List<Integer> current,List<List<Integer>> ans ){
+        if(index >= nums.length){
             ans.add(new ArrayList<>(current));
             return;
         }
-
-        // Take
         current.add(nums[index]);
-        generate(index + 1, nums, current, ans);
-        current.remove(current.size() - 1);
+        generate(index+1,nums,current,ans);
+        current.remove(current.size()-1);
 
-        // Don't take
-        int i = index + 1;
-
-        while (i < nums.length && nums[i] == nums[index]) {
+        int i=index+1;
+        while(i< nums.length && nums[i]==nums[index]){
             i++;
         }
+        generate(i,nums,current,ans);
 
-        generate(i, nums, current, ans);
     }
 }
